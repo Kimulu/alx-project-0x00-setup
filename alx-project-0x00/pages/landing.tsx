34c259750dc7cbaefc477a1b8490e819e6066b23
kimulu <kimulumichael@gmail.com>
2025-05-28 11:10:41 +0300

    add buttons to task 6

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -15,12 +15,17 @@ const Landing: React.FC = () => {
           <h2 className="font-semibold">Small Buttons</h2>
           <Button title="Small - Rounded Sm" styles="text-sm rounded-sm" />
           <Button title="Small - Rounded Md" styles="text-sm rounded-md" />
+          <Button title="Small - Rounded Full" styles="text-sm rounded-full" />
           <Button title="Small - Rounded Lg" styles="text-sm rounded-lg" />
         </div>
 
         <div className="space-y-2">
           <h2 className="font-semibold">Medium Buttons</h2>
           <Button title="Medium - Rounded Sm" styles="text-base rounded-sm" />
+          <Button
+            title="Medium - Rounded Full"
+            styles="text-base rounded-full"
+          />
           <Button title="Medium - Rounded Md" styles="text-base rounded-md" />
           <Button title="Medium - Rounded Lg" styles="text-base rounded-lg" />
         </div>
@@ -30,6 +35,7 @@ const Landing: React.FC = () => {
           <Button title="Large - Rounded Sm" styles="text-lg rounded-sm" />
           <Button title="Large - Rounded Md" styles="text-lg rounded-md" />
           <Button title="Large - Rounded Lg" styles="text-lg rounded-lg" />
+          <Button title="Large - Rounded Full" styles="text-lg rounded-full" />
         </div>
       </div>
     </>
